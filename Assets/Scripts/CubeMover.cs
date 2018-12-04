10c47f26bc30b2b98f2adad3a90a915f60207dd2
billloucks <loucks.bill@outlook.com>
2018-12-03 20:20:22 -0600

    TestCommit1
    TestCommit1

---
+++ Assets/Scripts/CubeMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical); //direction going
+
+        transform.position += movement * Time.deltaTime;
+    }
+}
